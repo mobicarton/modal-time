0fa575a0186cb1709968b4cedb6b94d0b11c2026
Damien Brun <makowildcat@gmail.com>
2017-01-22 23:34:54 -0500

    set Maze configuration

--- app/src/main/java/mobi/carton/modaltime/maze/MazeActivity.java
+++ app/src/main/java/mobi/carton/modaltime/maze/MazeActivity.java
@@ -12,6 +12,32 @@ public class MazeActivity extends CartonActivity {
 
 
     private GridView gridViewMaze;
+    private MazeAdapter mAdapter;
+
+
+    private int[] mMazeArray;
+    private int currentPosition;
+
+
+    private void initMazeArray() {
+        for (int i = 0; i < 16; i++) {
+            mMazeArray[i] = MazeAdapter.STATE_NEUTRAL;
+        }
+
+        mMazeArray[10] = MazeAdapter.STATE_START;
+        mMazeArray[15] = MazeAdapter.STATE_END;
+        mMazeArray[7] = MazeAdapter.STATE_POINT_BEFORE;
+        mMazeArray[0] = MazeAdapter.STATE_POINT_BEFORE;
+        mMazeArray[13] = MazeAdapter.STATE_POINT_BEFORE;
+    }
+
+
+    private void setPosition(int newPosition) {
+        mMazeArray[currentPosition] -= 1;
+        mMazeArray[newPosition] += 1;
+        currentPosition = newPosition;
+        mAdapter.notifyDataSetChanged();
+    }
 
 
     /*
@@ -26,7 +52,14 @@ public class MazeActivity extends CartonActivity {
 
         gridViewMaze = (GridView) findViewById(R.id.gridViewMaze);
 
-        MazeAdapter adapter = new MazeAdapter(this);
-        gridViewMaze.setAdapter(adapter);
+        mMazeArray = new int[16];
+        initMazeArray();
+
+        currentPosition = 10;
+        mMazeArray[currentPosition] += 1;
+
+        mAdapter = new MazeAdapter(this, mMazeArray);
+
+        gridViewMaze.setAdapter(mAdapter);
     }
 }
--- app/src/main/java/mobi/carton/modaltime/maze/MazeAdapter.java
+++ app/src/main/java/mobi/carton/modaltime/maze/MazeAdapter.java
@@ -2,29 +2,43 @@ package mobi.carton.modaltime.maze;
 
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import mobi.carton.modaltime.R;
+
+import static mobi.carton.modaltime.R.id.textViewNumber;
 
 
 public class MazeAdapter extends BaseAdapter {
 
 
+    public static final int STATE_NEUTRAL = 0;
+    public static final int STATE_START = 10;
+    public static final int STATE_END = 20;
+    public static final int STATE_POINT_BEFORE = 30;
+    private static final int STATE_POINT_AFTER = 40;
+
+
     private final Context mContext;
 
+    private int[] mMazeIntArray;
 
-    public MazeAdapter(Context context) {
+
+    public MazeAdapter(Context context, int[] mazeIntArray) {
         this.mContext = context;
+        this.mMazeIntArray = mazeIntArray;
     }
 
 
     @Override
     public int getCount() {
-        return 16;
+        return mMazeIntArray.length;
     }
 
 
@@ -42,14 +56,58 @@ public class MazeAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
         if (convertView == null) {
             final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
             convertView = layoutInflater.inflate(R.layout.item_maze, parent, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.mBackground = (RelativeLayout) convertView.findViewById(R.id.relativeLayoutBackground);
+            viewHolder.mNumber = (TextView) convertView.findViewById(textViewNumber);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        final TextView textViewNumber = (TextView)convertView.findViewById(R.id.textViewNumber);
-        textViewNumber.setText(String.valueOf(position + 1));
+        viewHolder.mNumber.setText(String.valueOf(position + 1));
+
+        switch (mMazeIntArray[position]) {
+            case STATE_START+1:
+            case STATE_NEUTRAL+1:
+            case STATE_END+1:
+            case STATE_POINT_AFTER+1:
+            case STATE_POINT_BEFORE+1:
+                viewHolder.mBackground.setBackgroundColor(ContextCompat.getColor(mContext, R.color.colorMazeActual));
+                break;
+
+            case STATE_START:
+                viewHolder.mBackground.setBackgroundColor(ContextCompat.getColor(mContext, R.color.colorMazeStart));
+                break;
+
+            case STATE_END:
+                viewHolder.mBackground.setBackgroundColor(ContextCompat.getColor(mContext, R.color.colorMazeEnd));
+                break;
+
+            case STATE_POINT_BEFORE:
+                viewHolder.mBackground.setBackgroundColor(ContextCompat.getColor(mContext, R.color.colorMazePointBefore));
+                break;
+
+            case STATE_POINT_AFTER:
+                viewHolder.mBackground.setBackgroundColor(ContextCompat.getColor(mContext, R.color.colorMazePointAfter));
+                break;
+
+            case STATE_NEUTRAL:
+            default:
+                viewHolder.mBackground.setBackgroundColor(ContextCompat.getColor(mContext, R.color.colorMazeNeutral));
+                break;
+        }
 
         return convertView;
+    }
+
+
+    private static class ViewHolder {
+        private RelativeLayout mBackground;
+        private TextView mNumber;
     }
 }
